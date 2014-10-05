7e0e52f51dffe210a5ded1fd87ef40b655d795fa
Jaeman Choi <claztec@gmail.com>
2014-10-05 14:15:04 +0900

    스태틱 파일 추가

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader, RequestContext
+
 
 def index(request):
-    return HttpResponse('Hello, world. ')
+    template = loader.get_template('users/index.html')
+    context = RequestContext(request, {
+
+    })
+    return HttpResponse(template.render(context))
 
 
 def info(request):
