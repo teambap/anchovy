3ea046734a8e060c1496973a5846a4cd1f480248
Jaeman Choi <claztec@gmail.com>
2014-10-05 15:24:51 +0900

    사용자 추가 페이지

--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponse
+from django import forms
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader, RequestContext
+from django.views.decorators.csrf import csrf_exempt
+from users.models import User
+
+
+class UserForm(forms.Form):
+    first_name = forms.CharField(label='First Name', max_length=30)
+    last_name = forms.CharField(label='Last Name', max_length=30)
+    email = forms.CharField(label='email', max_length=40)
+    user_id = forms.CharField(label='Id', max_length=30)
 
 
 def index(request):
@@ -14,9 +24,30 @@ def index(request):
 def info(request):
     return HttpResponse('Hello, world. ')
 
-
+@csrf_exempt
 def add(request):
-    return HttpResponse('Hello, world. ')
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            user_id = form.cleaned_data['user_id']
+            print("email:%s first_name:%s last_name:%s user_id:%s" % (email, first_name, last_name, user_id))
+
+            user = User()
+            user.email = email
+            user.last_name = last_name
+            user.first_name = first_name
+            user.user_id = user_id
+            user.save()
+
+            return HttpResponseRedirect('./')
+    else:
+        form = UserForm()
+
+
+    return render(request, 'users/add.html', {'form':form})
 
 
 
