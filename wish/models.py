7fe60b1453acf27ab7b226499aed66e5860c85ac
Jaeman Choi <claztec@gmail.com>
2014-11-29 17:54:59 +0900

    wish 추가

--- wish/models.py
+++ wish/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    user_id = models.IntegerField(max_length=11)
+    name = models.CharField(max_length=200)
+    link = models.URLField()
+    registered = models.DateField()
+    updated = models.DateField()
+    status = models.CharField(max_length=2)
--- wish/views.py
+++ wish/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 
-# Create your views here.
+
+def list(request):
+
+    context = RequestContext(request,
+                           {'request':request,
+                            'user': request.user})
+    return render_to_response('wish/list.html',
+                             context_instance=context)
