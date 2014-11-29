8c2c2e9177d6565062d30378eb9860eb58d64aad
Jaeman Choi <claztec@gmail.com>
2014-11-29 17:53:57 +0900

    wish url 추가

--- anchovy/urls.py
+++ anchovy/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/', include('users.urls')),
+    url(r'^wish/', include('wish.urls')),
     url(r'^social/', include('social.apps.django_app.urls', namespace='social')),
     url(r'^account/', include('django.contrib.auth.urls', namespace='auth')),
 
