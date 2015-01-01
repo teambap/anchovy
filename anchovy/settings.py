c50bf8c7e68808b34f1f3b14b195fec7759bcb59
Jaeman Choi <claztec@gmail.com>
2015-01-01 17:03:44 +0900

    static 경로 변경

--- anchovy/settings.py
+++ anchovy/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/wwish/'
+STATIC_URL = '/wish/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'anchovy-ui/dist')]
 
